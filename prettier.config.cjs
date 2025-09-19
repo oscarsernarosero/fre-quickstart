74562f1b554f1c3a5567316aa7cf21eb8f7948ce
Oscar Serna <oscar.eduardo.serna@gmail.com>
2025-09-19 15:08:51 -0500

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
